c97bf7b67c5a85d3a773b0af6d1a836349b7bdcb
JhovanVillaflores <49948216+JhovanVillaflores@users.noreply.github.com>
2020-06-22 00:14:14 +0800

    3rd commit

--- js/auth.js
+++ js/auth.js
@@ -4,15 +4,18 @@ $(document).ready( function(){
 
   $("#login-form").submit( function(e){
     e.preventDefault();
-    //var user = $("#username").val();
-  //  var pass = $("#password").val();
-  //  var loginData = {username:user,password:pass};
+      var user = $("#username").val();
+      var pass = $("#password").val();
+      var loginData = {
+        username:user,
+        password:pass
+      };
   //  console.log(JSON.stringify(loginData));
     $.ajax({
       url:apiUrl+'login-user.php.',
       method: "POST",
     //  data:JSON.stringify(loginData),
-      data:$("#login-form").serialize(),
+      data:loginData,
       dataType: "JSON",
       success: function(response){
         console.log(response);
